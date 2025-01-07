c428a3684373c70c601e7f243704e037ea319b57
Nadin <nadiya.kovaliova@gmail.com>
2025-01-07 16:25:11 +0000

    1

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    
