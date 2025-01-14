e9882d4b6865b5952757ce2fa906fa5e6a4a460e
Nadin <nadiya.kovaliova@gmail.com>
2025-01-14 16:06:32 +0000

    14/1

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,11 +4,40 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+class Category(models.Model):
+    categoryName = models.CharField(max_length=50)
 
 class AuctionListing(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     start_bid = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.URLField(blank=True, null=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
-    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+
+    def __str__(self):
+        return self.title
+
+
+
+
+class Bid(models.Model):
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    bid_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.bidder} bid {self.amount} on {self.listing.title}"
+
+
+class Comment(models.Model) :
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} commented on {self.listing.title}"
